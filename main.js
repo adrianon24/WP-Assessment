7a61cf73d3f7495097645de3d6e390186efe4074
adrianon <ontiveros.on24@gmail.com>
2022-05-16 09:23:50 -0700

    push files up

---
+++ main.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var myModal = new bootstrap.Modal(document.getElementById("myModal"));
+    myModal.show();
+});
+
+
+function myFunction() {
+    alert("I am an alert box!");
+}
