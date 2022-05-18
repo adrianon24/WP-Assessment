afcc796aec91a7e74e7c7aec4c029f6100534f29
adrianon <ontiveros.on24@gmail.com>
2022-05-18 00:00:30 -0700

    add new changes to all files

--- js/main.js
+++ js/main.js
@@ -1,9 +1,6 @@
+// Pop up banner
+
 document.addEventListener("DOMContentLoaded", function () {
     var myModal = new bootstrap.Modal(document.getElementById("myModal"));
     myModal.show();
 });
-
-
-function myFunction() {
-    alert("I am an alert box!");
-}
